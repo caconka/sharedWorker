777ff0268d1788b0db83e6e1f6c0dbdc6e410b3b
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-06-19 14:36:08 +0200

    hola mundo

--- shared.js
+++ shared.js
@@ -1,12 +1,12 @@
 var ws = null
 var url = "ws://merchant.qless.com:8000"
 self.addEventListener("connect", function(e) {
-	console.log('entro en connect self')
 	var port = e.ports[0]
 	port.addEventListener("message", function(e) {
 		if (e.data === "start") {
 			if (ws === null) {
-				console.log('entro para crear el socket')
+				console.log('entro en el ws');
+				port.postMessage('Hola mundo!');
 				ws = new WebSocket(url);
 				port.postMessage("started connection to " + url);
 			} else {
